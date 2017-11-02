798d0e88e65085b5f7a79e8c76485a57f2c7728b
Sergi <sergi@live.nl>
2017-11-02 17:54:50 +0100

    updated shader and VR test

---
+++ Assets/Flower_Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower_Trigger : MonoBehaviour {
+	public RayCastMouse RCM;
+	// Use this for initialization
+
+	void Start()
+	{
+		//RCM.RemoveColours (10);
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log ("COLLISION " + other.gameObject.name);
+		if (other.gameObject.tag == "Hands") {
+			RCM.RemoveColours (10);
+		}
+
+	}
+}
--- Assets/Scripts/RayCastMouse.cs
+++ Assets/Scripts/RayCastMouse.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RayCastMouse : MonoBehaviour {
-    public Camera _camera;
-    RaycastHit _hit;
-    Ray _ray;
     Vector3 _mousePos, _smoothPoint;
     public float _radius, _softness, _smoothSpeed, _scaleFactor;
 	// Use this for initialization
@@ -28,16 +25,27 @@ public class RayCastMouse : MonoBehaviour {
         }
         Mathf.Clamp(_radius, 0, 100);
         Mathf.Clamp(_softness, 0, 100);
-
-        _mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-        _ray = _camera.ScreenPointToRay(_mousePos);
-
-        if(Physics.Raycast(_ray, out _hit)) {
-            _smoothPoint = Vector3.MoveTowards(_smoothPoint, _hit.point, _smoothSpeed * Time.deltaTime);
-            Vector4 pos = new Vector4(-2.86f, -1.49f, 1.26f, 0);
-            Shader.SetGlobalVector("GLOBALmask_Position", pos);
-        }
+        Vector4 pos = new Vector4(-2.86f, -1.49f, 1.26f, 0);
+        Shader.SetGlobalVector("GLOBALmask_Position", pos);
         Shader.SetGlobalFloat("GLOBALmask_Radius", _radius);
         Shader.SetGlobalFloat("GLOBALmask_Softness", _softness);
     }
+
+	public void RemoveColours(float time)
+	{
+		StartCoroutine("RemoveColoursInTime", 10);
+	}
+
+	IEnumerator RemoveColoursInTime(float time)
+	{
+		while (time > 0) {
+			Debug.Log ("Time " + time);
+			Debug.Log ("Shader: " + Shader.GetGlobalFloat ("GLOBALmask_Radius"));
+			time = time -0.05f;
+			_radius = time;
+			yield return new WaitForSeconds (0.05f);
+		}
+
+	}
+
 }
